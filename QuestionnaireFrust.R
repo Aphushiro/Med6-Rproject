abe4dd1b5757e95405a8aebe3842d3bbcbda14c0
Linus Nørgaard Hollensberg <92723056+Bambalombo@users.noreply.github.com>
2024-05-21 09:17:58 +0200

    Update QuestionnaireFrust.R

--- QuestionnaireFrust.R
+++ QuestionnaireFrust.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 
 # Reshape data to long format
-justFrust_long <- justFrust %>%
+justFrust_long <- df %>%
   pivot_longer(cols = starts_with("Frust"), names_to = "Variable", values_to = "Value")
 
 # Calculate mean and standard error for each variable
@@ -15,11 +15,12 @@ summary_stats <- justFrust_long %>%
 ggplot() +
   geom_point(data = justFrust_long, aes(x = Variable, y = Value), position = position_dodge2(width = 0.5, preserve = "single"), color = "gray", size = 2, show.legend = FALSE) +
   geom_errorbar(data = summary_stats, aes(x = Variable, ymin = mean - std_error, ymax = mean + std_error), width = 0.1, color = "black") +
-  geom_point(data = summary_stats %>% filter(Variable == "Frust1"), aes(x = Variable, y = mean), size = 3, color = "#8B4513", show.legend = FALSE) +
-  geom_point(data = summary_stats %>% filter(Variable == "Frust2"), aes(x = Variable, y = mean), size = 3, color = "#008000", show.legend = FALSE) +
-  geom_point(data = summary_stats %>% filter(Variable == "Frust3"), aes(x = Variable, y = mean), size = 3, color = "#e5ca19", show.legend = FALSE) +
+  geom_point(data = summary_stats, aes(x = Variable, y = mean), size = 3, color = "black", show.legend = FALSE) +
+  #geom_point(data = summary_stats %>% filter(Variable == "Frust1"), aes(x = Variable, y = mean), size = 3, color = "#8B4513", show.legend = FALSE) +
+  #geom_point(data = summary_stats %>% filter(Variable == "Frust2"), aes(x = Variable, y = mean), size = 3, color = "#008000", show.legend = FALSE) +
+  #geom_point(data = summary_stats %>% filter(Variable == "Frust3"), aes(x = Variable, y = mean), size = 3, color = "#e5ca19", show.legend = FALSE) +
   geom_text(data = summary_stats, aes(x = Variable, y = mean, label = round(mean, 2)), vjust = -0.5, hjust = -0.5, color = "black") +
-  labs(title = "Frustation on Worst-Case States",
+  labs(title = "Frustation Experienced on Worst-Case States",
        x = "Games",
        y = "Frustration Rating") +
   scale_y_continuous(breaks = seq(-3, 3, 1), limits = c(-3, 3)) +
